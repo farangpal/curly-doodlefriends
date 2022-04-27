e586558dffa2758dc9b3bce5f699e71099cd6128
farangpal <98533096+farangpal@users.noreply.github.com>
2022-04-27 05:22:14 +0000

    Update app.js

--- media/app.js
+++ media/app.js
@@ -62,7 +62,7 @@ minusBtn.addEventListener('click',()=>{
 })
 //** end of input number spinner */
 
-checkoutBtn.style.display = "none !important"
+checkoutBtn.style.display = "none"
 
 connectBtn.addEventListener('click', async () => {
     if (ethereum) {
@@ -70,8 +70,8 @@ connectBtn.addEventListener('click', async () => {
         await ethereum.enable();
         initPayButton()
         statusp.innerHTML = 'Wallet connected'
-        connectBtn.style.display = "none !important"
-        checkoutBtn.style.display = "block !important"
+        connectBtn.style.display = "none"
+        checkoutBtn.style.display = "block"
       } catch (err) {
         console.log(err)
         statusp.innerHTML = 'Wallet access denied'
